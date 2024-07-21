89f27a7cf799df9f8013312d5ccb3f74e4934bfe
kalteon <102862962+kalteon@users.noreply.github.com>
2024-07-21 15:25:13 +0900

    Title: 아름다운 수, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/아름다운 수/beautiful-number.py
+++ 240721/아름다운 수/beautiful-number.py
@@ -1,22 +1,23 @@
 import sys
 
-# 1자리 남으면 1
-# 2자리 남으면 11, 22
-# 3자리 남으면 111, 122, 221, 333,
-# 4자리 남으면 1111, 1122, 2211, 2222, 1333, 3331 4444
 n = int(sys.stdin.readline())
-number_len = 0
-count = 0
-def count_beautiful_n_digit_number(current_len, count):
-    if current_len == n: 
-        return 1
-    elif current_len > n:
-        return 0
+l = []
+check_count = []
+n = 1
+def count_beautiful_n_digit_number():
+    if len(l) == n:
+        check_count.append(1)
+        return
+    elif len(l) > n:
+        return
     else:
         for i in range(1, 5):
-            current_len += i
-            count += count_beautiful_n_digit_number(current_len, count)
-            current_len -= i
-        return count
+            for j in range(i):
+                l.append(i)
+            count_beautiful_n_digit_number()
+            for j in range(i):
+                l.pop()
+        return
 
-print(count_beautiful_n_digit_number(number_len, count))
+count_beautiful_n_digit_number()
+print(sum(check_count))
