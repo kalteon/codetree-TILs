e96109069e48a6d76dec5f2c70e410201b5a2c3a
kalteon <102862962+kalteon@users.noreply.github.com>
2024-07-21 15:26:23 +0900

    Title: 아름다운 수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/아름다운 수/beautiful-number.py
+++ 240721/아름다운 수/beautiful-number.py
@@ -3,9 +3,10 @@ import sys
 n = int(sys.stdin.readline())
 l = []
 check_count = []
-n = 1
+n = 2
 def count_beautiful_n_digit_number():
     if len(l) == n:
+        print()
         check_count.append(1)
         return
     elif len(l) > n:
